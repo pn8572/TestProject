185a23ac4b910b52f8c2159f1535f3f11be07346
prasanth.pillai <prasanth.pillai@DT-LGC08328.corp.lgc-group.com>
2017-01-16 18:05:34 +0000

    intrim

--- src/com/task/number/NumberToWords.java
+++ src/com/task/number/NumberToWords.java
@@ -1,5 +1,12 @@
 package com.task.number;
 
 public class NumberToWords {
+	
+	public String displayWords;
+
+	public String convert(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
