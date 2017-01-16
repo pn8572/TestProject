f2333b699a319a706bad30ba27531e009b234fb2
prasanth.pillai <prasanth.pillai@DT-LGC08328.corp.lgc-group.com>
2017-01-16 18:05:57 +0000

    intrim

--- src/test/com/task/number/NumberToWordsTest.java
+++ src/test/com/task/number/NumberToWordsTest.java
@@ -1,5 +1,7 @@
 package test.com.task.number;
 
+import org.junit.Test;
+
 import com.task.number.NumberToWords;
 
 import junit.framework.TestCase;
@@ -7,6 +9,7 @@ import junit.framework.TestCase;
 public class NumberToWordsTest  extends TestCase {
 
 	NumberToWords NumberToWordsObj;
+	String displayWords;
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -17,6 +20,14 @@ public class NumberToWordsTest  extends TestCase {
     protected void tearDown() throws Exception {
         super.tearDown();
     }
+	
+	@Test
+	public void testAcceptance() throws Exception {
+	  
+		displayWords = NumberToWordsObj.convert(12);
+        assertNotNull(NumberToWordsObj.displayWords);
+        assertEquals("twelve", displayWords);
+	}
 	
 	
 }
