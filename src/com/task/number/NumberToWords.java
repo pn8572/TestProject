2d96d637e7c0ae3861d21f72432d0c13089fdd50
prasanth.pillai <prasanth.pillai@DT-LGC08328.corp.lgc-group.com>
2017-01-16 18:11:40 +0000

    intrim

--- src/com/task/number/NumberToWords.java
+++ src/com/task/number/NumberToWords.java
@@ -5,8 +5,52 @@ public class NumberToWords {
 	public String displayWords;
 
 	public String convert(int i) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
-
+    private static final String[] numNames = {
+        "",
+        " one",
+        " two",
+        " three",
+        " four",
+        " five",
+        " six",
+        " seven",
+        " eight",
+        " nine",
+        " ten",
+        " eleven",
+        " twelve",
+        " thirteen",
+        " fourteen",
+        " fifteen",
+        " sixteen",
+        " seventeen",
+        " eighteen",
+        " nineteen"
+    };
+    
+    private static final String[] tensPositions = {
+	        "",
+	        " ten",
+	        " twenty",
+	        " thirty",
+	        " forty",
+	        " fifty",
+	        " sixty",
+	        " seventy",
+	        " eighty",
+	        " ninety"
+	};
+    
+    private static final String[] largerPositions = {
+	        "",
+	        " thousand",
+	        " million",
+	        " billion",
+	        " trillion",
+	        " quadrillion",
+	        " quintillion"
+	};
 }
