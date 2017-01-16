5a70d0fe961d6d9382939aa7a950ed159488939d
RA P. Pillai <RA.P.Pillai@corp.lgc-group.com>
2017-01-16 09:39:44 +0000

    Initial Commit

---
+++ src/java/com/task/sorting/Sorting.java
@@ -0,0 +1,18 @@
+package java.com.task.sorting;
+
+/**
+ * 
+ * @author prasanth.pillai
+ *
+ */
+public class Sorting {
+
+	/*
+	 * Initial version
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/task/sorting/SortingTest.java
@@ -0,0 +1,10 @@
+package test.java.com.task.sorting;
+
+/**
+ * 
+ * @author prasanth.pillai
+ *
+ */
+public class SortingTest {
+
+}
